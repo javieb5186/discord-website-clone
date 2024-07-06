59316876598de1f87fa9ad1d2f1a7b1e0c949537
Javier B <javieb5186@gmail.com>
2024-07-05 20:50:11 -0600

    Add Platforms component

---
+++ src/app/components/PlatformsContent.tsx
@@ -0,0 +1,23 @@
+import SectionBackground from "./SectionBackground";
+import SectionCard from "./SectionCard";
+import platformsImg from "assets/platforms.png";
+
+export default function PlatformsContent() {
+  return (
+    <SectionBackground
+      bgGradient={"radial-gradient(at 100% 100%, #3255eb, #000027)"}
+    >
+      <SectionCard
+        inverse={false}
+        image={{
+          src: platformsImg,
+          alt: "Discord can be used on multiple platforms",
+        }}
+        title={"WHEREVER YOU GAME, HANG OUT HERE"}
+        body={
+          "On your PC, phone, or console, you can still hang out on Discord. Easily switch between devices and use tools to manage multiple group chats with friends."
+        }
+      />
+    </SectionBackground>
+  );
+}
