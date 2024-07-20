498e3c95e93efe9d129c07ffd087ca1f654511f6
Javier B <javieb5186@gmail.com>
2024-07-20 13:06:20 -0600

    Complete responsive plan for nitro page

--- src/app/nitro/Plan.tsx
+++ src/app/nitro/Plan.tsx
@@ -1,8 +1,11 @@
+"use client";
+import { Fragment, useContext } from "react";
 import { Box, Stack, Grid, Typography, Divider } from "@mui/material";
 import { SimpleButton } from "globalComponents";
 import { RBox } from "globalComponents";
 import CheckmarkIcon from "icons/CheckmarkIcon";
 import XMarkIcon from "icons/XMarkIcon";
+import { ScreenContext } from "store/ScreenContext";
 
 const features = [
   {
@@ -116,6 +119,7 @@ const features = [
 ];
 
 export default function Plan() {
+  const { laptop, desktop } = useContext(ScreenContext);
   return (
     <RBox bgColor="white">
       <Box
@@ -127,116 +131,210 @@ export default function Plan() {
         justifyContent="center"
         alignContent="center"
       >
-        <Stack position="relative" width="90%">
+        <Stack width="90%">
           <Typography
             component="h2"
-            variant="h2"
+            fontSize={{ xs: "1.75rem", sm: "2.25rem", lg: "2.75rem" }}
             textAlign="center"
             my="3rem"
-            px="13rem"
+            px={{ xs: "0rem", lg: "13rem" }}
           >
             Pick the plan that works best for you
           </Typography>
-          <Grid container marginBottom="1rem">
-            <Grid item xs>
-              <Typography component="h4" variant="h4">
-                Feature
+          <Stack spacing="1rem" marginBottom="1rem" position="relative">
+            <Box
+              height="100%"
+              position="absolute"
+              top={0}
+              right={0}
+              border="1px solid #8b46c3"
+              borderRadius="1rem"
+              width={{ xs: "4rem", md: "10rem" }}
+            >
+              <Typography
+                className="white-txt"
+                display={{ xs: "none", md: "block" }}
+                textAlign="center"
+                position="absolute"
+                width={{ xs: "4rem", md: "6rem" }}
+                top="-.5rem"
+                left={0}
+                right={0}
+                mx="auto"
+                borderRadius=".4rem"
+                sx={{
+                  backgroundImage: "linear-gradient(to left, #ab5b8e, #8b46c3)",
+                }}
+                fontSize=".7rem"
+              >
+                Popular
               </Typography>
-            </Grid>
-            <Grid item xs="auto">
-              <Stack textAlign="center">
-                <Typography component="h4" variant="h4" width="10rem">
+            </Box>
+            <Grid container marginBottom="1rem">
+              <Grid item xs>
+                <Typography
+                  display={{ xs: "none", md: "block" }}
+                  component="h4"
+                  fontSize={{ md: "1.5rem", lg: "2rem" }}
+                >
+                  Feature
+                </Typography>
+              </Grid>
+              <Grid item xs="auto">
+                <Stack textAlign="center">
+                  <Typography
+                    component="h4"
+                    fontSize="2rem"
+                    width="10rem"
+                    display={{ xs: "none", md: "block" }}
+                  >
+                    Nitro
+                  </Typography>
+                  <Typography
+                    fontSize={{ xs: "1.25rem", md: "1rem" }}
+                    width={{ xs: "4rem", md: "10rem" }}
+                  >
+                    Basic
+                  </Typography>
+                </Stack>
+              </Grid>
+              <Grid item xs="auto" position="relative">
+                <Typography
+                  textAlign="center"
+                  component="h4"
+                  fontSize={{ xs: "1.25rem", md: "2rem" }}
+                  width={{ xs: "4rem", md: "10rem" }}
+                >
                   Nitro
                 </Typography>
-                <Typography>Basic</Typography>
-              </Stack>
+              </Grid>
             </Grid>
-            <Grid item xs="auto" position="relative">
-              <Typography
-                textAlign="center"
-                component="h4"
-                variant="h4"
-                width="10rem"
-              >
-                Nitro
-              </Typography>
-              <Box
-                position="absolute"
-                top="-1.25rem"
-                left={0}
-                right={0}
-                border="1px solid red"
-                borderRadius="1rem"
-              >
-                <Box width="10rem" height="75rem" position="relative"></Box>
-              </Box>
-              <Typography
-                textAlign="center"
-                position="absolute"
-                width="8rem"
-                top="-2rem"
-                left={0}
-                right={0}
-                margin="0 auto"
-                borderRadius=".25rem"
-                sx={{ backgroundColor: "pink" }}
-                fontSize=".75rem"
-              >
-                Most Popular
-              </Typography>
-            </Grid>
-          </Grid>
-          <Divider sx={{ marginBottom: "1rem" }} />
-          <Stack spacing="1rem" marginBottom="3rem" divider={<Divider />}>
+            <Divider sx={{ marginBottom: "1rem" }} />
             {features.map(({ id, description, compare1, compare2 }) => {
               const comparisons = [compare1, compare2];
               return (
-                <Grid container key={id}>
-                  <Grid item xs>
-                    <Typography>{description}</Typography>
+                <Fragment key={id}>
+                  <Grid container>
+                    <Grid item xs>
+                      <Typography fontSize={{ xs: ".9rem", md: "1rem" }}>
+                        {description}
+                      </Typography>
+                    </Grid>
+                    {comparisons.map((compare, index) => {
+                      return (
+                        <Grid
+                          key={index}
+                          item
+                          xs="auto"
+                          textAlign="center"
+                          alignContent="center"
+                        >
+                          {typeof compare === "boolean" ? (
+                            <Box
+                              width={{ xs: "4rem", md: "10rem" }}
+                              display="flex"
+                              justifyContent="center"
+                            >
+                              {compare ? (
+                                <CheckmarkIcon width="1rem" />
+                              ) : (
+                                <XMarkIcon width="1rem" color="darkgray" />
+                              )}
+                            </Box>
+                          ) : (
+                            <Typography
+                              fontSize={{ xs: ".8rem", md: "1rem" }}
+                              width={{ xs: "4rem", md: "10rem" }}
+                            >
+                              {compare}
+                            </Typography>
+                          )}
+                        </Grid>
+                      );
+                    })}
                   </Grid>
-                  {comparisons.map((compare, index) => {
-                    return (
-                      <Grid key={index} item xs="auto" textAlign="center">
-                        {typeof compare === "boolean" ? (
-                          <Box
-                            width="10rem"
-                            display="flex"
-                            justifyContent="center"
-                          >
-                            {compare ? (
-                              <CheckmarkIcon width="1rem" />
-                            ) : (
-                              <XMarkIcon width="1rem" color="darkgray" />
-                            )}
-                          </Box>
-                        ) : (
-                          <Typography width="10rem">{compare}</Typography>
-                        )}
-                      </Grid>
-                    );
-                  })}
-                </Grid>
+                  <Divider sx={{ marginBottom: "1rem" }} />
+                </Fragment>
               );
             })}
-            <Grid container>
-              <Grid item xs></Grid>
-              <Grid item xs="auto">
-                <Box display="flex" justifyContent="center" width="10rem">
-                  <SimpleButton variant="contained" sx={{ fontSize: ".75rem" }}>
-                    Subscribe Basic
-                  </SimpleButton>
-                </Box>
-              </Grid>
-              <Grid item xs="auto">
-                <Box display="flex" justifyContent="center" width="10rem">
-                  <SimpleButton variant="contained" sx={{ fontSize: ".75rem" }}>
-                    Subscribe Nitro
-                  </SimpleButton>
-                </Box>
-              </Grid>
-            </Grid>
           </Stack>
+          <Grid container marginBottom="3rem">
+            {laptop || desktop ? (
+              <>
+                <Grid item md></Grid>
+                <Grid item md="auto" alignContent="center">
+                  <Box
+                    display="flex"
+                    justifyContent="center"
+                    width={{ xs: "100%", md: "10rem" }}
+                  >
+                    <SimpleButton
+                      variant="contained"
+                      sx={{ fontSize: ".75rem" }}
+                    >
+                      Subscribe Basic
+                    </SimpleButton>
+                  </Box>
+                </Grid>
+                <Grid item md="auto" alignContent="center">
+                  <Box
+                    display="flex"
+                    justifyContent="center"
+                    width={{ xs: "100%", md: "10rem" }}
+                  >
+                    <SimpleButton
+                      variant="contained"
+                      sx={{ fontSize: ".75rem" }}
+                    >
+                      Subscribe Nitro
+                    </SimpleButton>
+                  </Box>
+                </Grid>
+              </>
+            ) : (
+              <>
+                <Grid item xs={4}>
+                  <Typography fontSize="2rem">Nitro</Typography>
+                  <Typography>Basic</Typography>
+                </Grid>
+                <Grid item xs={8} md="auto" alignContent="center">
+                  <Box
+                    display="flex"
+                    justifyContent="center"
+                    width={{ xs: "100%", md: "10rem" }}
+                  >
+                    <SimpleButton
+                      fullWidth
+                      variant="contained"
+                      sx={{ fontSize: ".75rem" }}
+                    >
+                      Subscribe Basic
+                    </SimpleButton>
+                  </Box>
+                </Grid>
+                <Grid item xs={4}>
+                  <Stack>
+                    <Typography fontSize="2rem">Nitro</Typography>
+                  </Stack>
+                </Grid>
+                <Grid item xs={8} md="auto" alignContent="center">
+                  <Box
+                    display="flex"
+                    justifyContent="center"
+                    width={{ xs: "100%", md: "10rem" }}
+                  >
+                    <SimpleButton
+                      fullWidth
+                      variant="contained"
+                      sx={{ fontSize: ".75rem" }}
+                    >
+                      Subscribe Nitro
+                    </SimpleButton>
+                  </Box>
+                </Grid>
+              </>
+            )}
+          </Grid>
         </Stack>
       </Box>
     </RBox>
