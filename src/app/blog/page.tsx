2dc377726cee13eb8e8b9c4cb94b33c753992020
Javier B <javieb5186@gmail.com>
2024-08-02 19:29:29 -0600

    Add Navbar to blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,14 @@
+import { Box } from "@mui/material";
+import NavBar from "./NavBar";
+import Footer from "app/components/Footer";
+import "./page.css";
+
 export default function Blog() {
-  return <div>blog</div>;
+  return (
+    <>
+      <NavBar />
+      <Box height={"50rem"} width={"100%"} bgcolor="whitesmoke"></Box>
+      <Footer />
+    </>
+  );
 }
